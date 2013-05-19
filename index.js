3b70e598c3479f8f4393cab696102a8aca98e8b2
Enrico Marino <enrico.marino@email.com>
2013-05-19 18:08:56 +0200

    Add path_to_regexp

--- index.js
+++ index.js
@@ -8,6 +8,15 @@
 module.exports = Route;
 
 /**
+ * Regexp
+ */
+
+var escape_regexp = /[\-{}\[\]+?.,\\\^$|#\s]/g;
+var optional_param = /\((.*?)\)/g;
+var named_param = /:(\w+)/g;
+var splat_param = /\*\w+/g;
+
+/**
  * @constructor Router
  * @description Create a router.
  * @api public
@@ -17,5 +26,35 @@ function Route (path) {
   if (!(this instanceof Route)) {
     return new Route(path);
   }
+
+  var result = path_to_regexp(path);
+
   this.path = path;
+  this.keys = result.keys;
+  this.regexp = result.regexp;
+}
+
+function path_to_regexp (path, keys) {
+  keys = keys || [];
+
+  path = path
+    .replace(/[\-{}\[\]+?.,\\\^$|#\s]/g, function (match) {
+      return '\\$&';
+    })
+    .replace(/\((.*?)\)/g, function (match, key) {
+      keys.push(key);
+      return '([^\/]+)?';
+    })
+    .replace(/:(\w+)/g, function (match, key) {
+      keys.push(key);
+      return '([^\/]+)';
+    })
+    .replace(/\*\w+/g, function (match, key) {
+      keys.push(key);
+      return '(.*?)';
+    };
+  
+  regexp = new RegExp('^' + path + '$');
+
+  return { regexp: regexp, keys: keys };
 }
